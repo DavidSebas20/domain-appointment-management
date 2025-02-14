bdc557087faa1ce6d0bb9b6f9f48cf2168b32943
DavidSebas20 <davidsebastian20@outlook.com>
2025-02-14 04:04:58 +0000

    ✨ add GitHub Actions workflow for syncing microservice with domain repository; configure CORS and OpenAPI support; update appointment API path and add health check endpoint

---
+++ ms-createappointment/src/main/java/com/example/createappointment/controllers/AppointmentController.java
@@ -0,0 +1,36 @@
+package com.example.createappointment.controllers;
+
+
+import com.example.createappointment.entity.Appointment;
+import com.example.createappointment.service.AppointmentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/create-appointment/appointments")
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @Autowired
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    // Crear una nueva cita
+    @PostMapping
+    public ResponseEntity<Appointment> createAppointment(@RequestBody Appointment appointment) {
+        Appointment createdAppointment = appointmentService.createAppointment(appointment);
+        return new ResponseEntity<>(createdAppointment, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("Healthy");
+    }
+
+
+}
